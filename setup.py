45a28ca07381c7119918901ff1e370fa8df154d1
Ricky-Hao <a471558277@gmail.com>
2019-03-22 20:52:03 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='V2ray.Stats',
+    version='1.0',
+    url='https://github.com/Ricky-Hao/V2Ray.Stats',
+    license='Apache License 2.0',
+    author='Ricky Hao',
+    description='Collect V2Ray traffic stats by API.',
+    package=['v2ray_stats'],
+    install_requires=[
+        'schedule',
+        'texttable'
+    ]
+)
