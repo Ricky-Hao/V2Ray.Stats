8a594e8418e3e8fc4d2e71c536bac76c82d4ddf5
Ricky-Hao <a471558277@gmail.com>
2019-03-22 19:11:37 +0800

    Fix collector sql error.

--- v2ray_stats/collector.py
+++ v2ray_stats/collector.py
@@ -23,11 +23,11 @@ def collect_traffic_stats(db: str, server: str, reset: bool = True):
     cursor = connection.cursor()
     for stats in stats_list:
         if stats['bound'] == 'downlink':
-            sql = 'INSERT INTO outbound(email, traffic) VALUES ({0}, {1})'.format(stats['email'], int(stats['value']))
+            sql = 'INSERT INTO outbound(email, traffic) VALUES ("{0}", {1})'.format(stats['email'], int(stats['value']))
             cursor.execute(sql)
             V2RayLogger.debug(sql)
         elif stats['bound'] == 'uplink':
-            sql = 'INSERT INTO inbound(email, traffic) VALUES ({0}, {1})'.format(stats['email'], int(stats['value']))
+            sql = 'INSERT INTO inbound(email, traffic) VALUES ("{0}", {1})'.format(stats['email'], int(stats['value']))
             cursor.execute(sql)
             V2RayLogger.debug(sql)
     cursor.close()
